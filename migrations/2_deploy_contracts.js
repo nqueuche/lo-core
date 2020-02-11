1e3433f85295bf472c6ce240db1f24568c2119cc
Nina <n.queuche@gmail.com>
2020-02-11 15:57:36 +0100

    add billofsale contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,13 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
 const TutorialToken = artifacts.require("TutorialToken");
 const ComplexStorage = artifacts.require("ComplexStorage");
+const BillOfSale = artifacts.require("BillOfSale");
+
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
+  deployer.deploy(BillOfSale
+  );
 };
